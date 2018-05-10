f71e1cfab2927dcaf1370f063b58bf6fb5159896
Ilya <rewasd91@mail.ru>
2018-05-10 10:11:27 +0300

    fixes

--- app/blocks/card/card.js
+++ app/blocks/card/card.js
@@ -1,4 +1,4 @@
-/* eslint-disable prefer-arrow-callback */
+/* eslint-disable prefer-arrow-callback, no-inner-declarations */
 const $ = window.$;
 
 export default function card() {
@@ -30,5 +30,98 @@ export default function card() {
       form.children('.card-reviewform__inner').slideDown(500);
     }
   });
+
+  function validateImage(file, elem) {
+    if (file) {
+      for (let i = 0; i < file.length; i += 1) {
+        const validSize = $(elem).attr('data-correct-size');
+        const validWidth = $(elem).attr('data-correct-resolution').split('x')[0];
+        const validHeight = $(elem).attr('data-correct-resolution').split('x')[1];
+        const errorEl = $(elem).next('.card-reviewform__file-error');
+        errorEl.text('');
+        $(elem).removeAttr('data-error');
+
+        if (file[i].type.split('/')[0] !== 'image') {
+          errorEl.text('Файл не является изображением');
+          break;
+        }
+
+        if (file[i].size > validSize) {
+          errorEl.text('Размер файла превышает допустимый');
+          break;
+        }
+
+        function validateResolution(callback) {
+          const reader = new FileReader();
+          reader.readAsDataURL(file[i]);
+          reader.onload = function (e) {
+            const image = new Image();
+            image.src = e.target.result;
+            image.onload = function () {
+              const height = this.height;
+              const width = this.width;
+              if (height > validHeight && width > validWidth) {
+                const previews = $(elem).find('.card-reviewform__file-previews');
+                $(elem).find('.card-reviewform__file-preview.is-hidden').clone(true).appendTo(previews)
+                  .removeClass('is-hidden')
+                  .attr('data-file', i)
+                  .find('img')
+                  .attr('src', e.target.result);
+                $(elem).append(`<input type="hidden" name="file[${i}]" value="${e.target.result}">`);
+                callback(false);
+              } else {
+                errorEl.text('Разрешение файла меньше допустимого');
+                callback(true);
+              }
+            };
+          };
+        }
+
+        validateResolution(function (err) {
+          if (err) {
+            $(elem).attr('data-error', 'true');
+          }
+        });
+
+        if ($(elem).attr('data-error')) {
+          break;
+        }
+      }
+    }
+  }
+
+  $('.card-reviewform__file-label input').on('change', function () {
+    const dataEl = $(this).parents('.card-reviewform__file');
+    const previews = dataEl.find('.card-reviewform__file-preview:not(.is-hidden)');
+    previews.remove();
+    dataEl.find('input[type="hidden"]').remove();
+    validateImage(this.files, dataEl);
+  });
+
+  $('.card-reviewform__file').on('drag dragstart dragend dragover dragenter dragleave drop', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  })
+  .on('dragover dragenter', function () {
+    $(this).addClass('dragover');
+  })
+  .on('dragleave dragend drop', function () {
+    $(this).removeClass('dragover');
+  })
+  .on('drop', function (e) {
+    const droppedFiles = e.originalEvent.dataTransfer.files;
+    const dataEl = $(this);
+    const previews = dataEl.find('.card-reviewform__file-preview:not(.is-hidden)');
+    previews.remove();
+    dataEl.find('input[type="hidden"]').remove();
+    validateImage(droppedFiles, dataEl);
+  });
+
+  $(document).on('click', '.card-reviewform__file-preview-close', function (e) {
+    e.preventDefault();
+    const fileIndex = $(this).parent().attr('data-file');
+    $(this).parents('.card-reviewform__file').find(`input[name="file[${fileIndex}]"]`).remove();
+    $(this).parent().remove();
+  });
 }
-/* eslint-enable prefer-arrow-callback */
+/* eslint-enable prefer-arrow-callback, no-inner-declarations */
